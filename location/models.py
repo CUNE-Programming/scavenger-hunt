35e027880fbe39a3111fee4956eb215dea707cf9
Reagan Zierke <reaganzierke@gmail.com>
2025-01-28 15:31:30 -0600

    created model, still has error migrating

--- location/models.py
+++ location/models.py
@@ -4,3 +4,12 @@ from django.db import models
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page
+
+
+class LocationPage(Page):
+    name = models.CharField(max_length=255)
+    description = RichTextField(blank=True)
+    hint = RichTextField(blank=True)
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + ["name", "description", "hint"]
