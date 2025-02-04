bad66eb5b71778c2fc126ccdf838334f072fe761
Reagan Zierke <reaganzierke@gmail.com>
2025-02-04 15:19:07 -0600

    location model edit

--- location/models.py
+++ location/models.py
@@ -9,7 +9,21 @@ from wagtail.models import Page
 class LocationPage(Page):
     name = models.CharField(max_length=255)
     description = RichTextField(blank=True)
+    location_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     hint = RichTextField(blank=True)
+    hint_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     # modify your content_panels:
-    content_panels = Page.content_panels + [FieldPanel('name'), FieldPanel('description'), FieldPanel('hint')]
+    content_panels = Page.content_panels + [FieldPanel('name'), FieldPanel('description'), FieldPanel('location_image'), FieldPanel('hint'), FieldPanel('hint_image')]
